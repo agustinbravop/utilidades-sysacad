31e9627d5ad6616fe4b62d0946925335c081bb70
Agustín Bravo Pérez <anbravoperez@gmail.com>
2024-02-27 20:42:11 -0300

    intento de mejorar el FOUC de la imagen de fondo

--- js/background_image.js
+++ js/background_image.js
@@ -1,3 +1,6 @@
+const DEFAULT_DATA_URL =
+  "https://sysacadweb.frre.utn.edu.ar/static/img/back3.png";
+
 /** Establece la imagen de fondo del SYSACAD. */
 function setBackgroundImage(dataUrl) {
   if (dataUrl) {
@@ -5,14 +8,22 @@ function setBackgroundImage(dataUrl) {
     document.body.style.backgroundImage = `url('${dataUrl}')`;
   } else {
     // No hay fondo personalizado, así que se vuelve al original.
-    document.body.style.backgroundImage =
-      "url(https://sysacadweb.frre.utn.edu.ar/static/img/back3.png)";
+    document.body.style.backgroundImage = `url('${DEFAULT_DATA_URL}')`;
   }
 }
 
-// Inicialización (corre una sola vez cuando la página carga).
+// Es lo antes posible que se puede inicializar la imagen de fondo en el CSS.
+// Minimiza el tiempo del flash en que el fondo default se muestra, pero parece ser
+// imposible eliminarlo por completo, dado que el CSS se carga antes que el JavaScript.
 chrome.storage.local.get("imageDataUrl").then((items) => {
-  setBackgroundImage(items.imageDataUrl);
+  document.documentElement.insertAdjacentHTML(
+    "beforeend",
+    `<style>
+      body {
+        background-image: url('${items.imageDataUrl || DEFAULT_DATA_URL}');
+      }
+    </style>`
+  );
 });
 
 // Event listener (corre cada vez que cambia el atributo `imageDataUrl`).
